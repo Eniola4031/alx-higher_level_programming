f11d0364f25c3b882a2ee6123849001027552673
eniola4031 <agboola.eniola4031@gmail.com>
2024-03-22 14:29:43 -0700

    init

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,18 @@
+87% of storage used … If you run out, you can't create, edit, and upload files. Get 100 GB of storage for ₦950.00 ₦240.00/month for 3 months.
+#!/usr/bin/python3
+"""  lists all states from the database hbtn_0e_0_usa """
+import MySQLdb
+import sys
+
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states")
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+    cur.close()
+    db.close()
+
